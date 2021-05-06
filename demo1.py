9f59efb2a72a9803f18bf42fc52fd9bdfbba1b39
Rory William <64885374+TheRoryWillAim@users.noreply.github.com>
2021-05-06 16:20:20 +0530

    demo1

---
+++ demo1.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("hello streamlit")
+
+st.header("Header")
+
+st.subheader("Subheader")
+st.text("text")
+
+st.markdown(""" # h1 tag
+## h2 tag
+### h3 tag
+:sunglasses:
+** bold **
+_ italic _
+""",True)
+
+st.latex(r''' a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} = 
+\sum_{k=0}^{n-1} ar^k =
+a \left(\frac{1-r^{n}}{1-r}\right)''')
+
+st.write(" # mayank","kilhor","python")
+st.write(st)
+st.write(sum)
