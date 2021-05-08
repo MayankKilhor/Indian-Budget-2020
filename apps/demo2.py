ffd2b646dfbd36c823d0df3f8f4fb5dfcabee2ae
Rory William <64885374+TheRoryWillAim@users.noreply.github.com>
2021-05-08 19:38:51 +0530

    module done

--- apps/demo2.py
+++ apps/demo2.py
@@ -8,6 +8,7 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.tools as tls
 
+
 def app():
     df = st.cache(pd.read_csv)("FederalBudgetShares1941_2020.csv")
     is_check = st.checkbox("Display Data")
@@ -34,17 +35,48 @@ def app():
     sns.barplot(df.columns[1:],df[df.columns[1:]].sum(axis=0))
     plt.plot(df.columns[1:], rolling_mean, color='red',label='moving average')
     plt.xticks(rotation='vertical')
-    # st.pyplot()
     st.write(fig)
-    # tls.mpl_to_plotly(fig)
-    # st.write(fig)
-    # st.plotly_chart(fig, use_container_width=True)
+ 
+    # Figure 2
 
+    z = df.transpose().iloc[1:]
+    fig4 = go.Figure()
+    fig4.add_trace(go.Bar(x=z.index,y=df[z.index].sum(axis=0),orientation='v',
+            marker=dict(line=dict(color='rgb(248, 248, 249)', width=1))))
+    fig4.add_trace(go.Scatter(x=z.index,y=rolling_mean,name="Rolling Mean",line=dict(width=4)))
 
-    # z = df.transpose().iloc[1:]
-    # plt.figure(figsize=(20,10))
-    # for cat in df['Category']:
-    #     print(z[cat])
-    #     plt.plot(z.index, z[cat], label=cat)
-    #     plt.xticks(rotation='vertical')
-    # plt.legend()
+    st.plotly_chart(fig4)
+
+    # Figure 3
+    colors = ['rgba(38, 24, 74, 0.8)', 'rgba(71, 58, 131, 0.8)',
+          'rgba(122, 120, 168, 0.8)', 'rgba(164, 163, 204, 0.85)',
+          'rgba(190, 192, 213, 1)']
+
+    z = df.transpose().iloc[72:82]
+    fig3 = go.Figure()
+    fig3.add_trace(go.Bar(x=z.index,y=df[z.index].sum(axis=0),orientation='v',
+            marker=dict(line=dict(color='rgb(248, 248, 249)', width=1))))
+    fig3.add_trace(go.Scatter(x=z.index,y=rolling_mean[72:],name="Rolling Mean",line=dict(width=4)))
+    st.plotly_chart(fig3)
+
+    # Figure 4  
+    fig5 = go.Figure()
+    z = df.transpose().iloc[1:]
+
+    j=0
+    for i in z.index:
+        # st.write(z.index)
+        fig5.add_trace(go.Scatter(x=df['Category'],y=df[i],name=str(i),line=dict(width=4)))
+        j=j+1
+    st.plotly_chart(fig5)
+
+    # Figure 5  
+    fig2 = go.Figure()
+    z = df.transpose().iloc[72:82]
+
+    j=0
+    for i in z.index:
+        # st.write(z.index)
+        fig2.add_trace(go.Scatter(x=df['Category'],y=df[i],name=str(i),line=dict(width=4)))
+        j=j+1
+    st.plotly_chart(fig2, use_container_width=True)
